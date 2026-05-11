4c58d0933cab31e446294e6c2ca0c9f649242a1d
スカイ <2247032@ecc.ac.jp>
2026-05-11 13:13:43 +0900

    iii

--- Assets/script/bosatk.cs
+++ Assets/script/bosatk.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class bosatk : MonoBehaviour
 {
-    public GameObject Danmaku;
+    public GameObject[] Danmaku;
     private float timer=0.0f;
     public float time=0;
     public int pattern = 0;
@@ -21,17 +21,28 @@ public class bosatk : MonoBehaviour
         if (time < timer&& pattern==0)
         {
             timer = 0;
-            Instantiate(Danmaku, new Vector2(Random.Range(8f, -8.5f), 1), Quaternion.Euler(0, 0, Random.Range(0f, 360f)));
+            time = 1.4f;
+            Instantiate(Danmaku[0], transform.position, Quaternion.Euler(0, 0, Random.Range(90f, 270f)));
+            Instantiate(Danmaku[0], transform.position, Quaternion.Euler(0, 0, Random.Range(270,450)));
         }
         if (time < timer && pattern == 1)
         {
             timer = 0;
+            time = 3;
             rnd = Random.Range(2.0f, -8.5f);
             for (int i = 0; i <= 16; i++) 
             {
-                
-                Instantiate(Danmaku, new Vector2(rnd,2f), Quaternion.Euler(0, 0, i * 22.5f));
+
+                Instantiate(Danmaku[1], new Vector2(rnd,2f), Quaternion.Euler(0, 0, i * 22.5f));
             }
         }
+        if (time < timer && pattern == 2)
+        {
+            timer = 0;
+            time = 0.2f;
+            Instantiate(Danmaku[2], new Vector2(Random.Range(-8.0f,8.0f), 5f), transform.rotation);
+            
+        }
+
     }
 }
