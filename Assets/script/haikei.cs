e66f6489afea7c76accebfa7041cda1011aa5b7a
スカイ <2247032@ecc.ac.jp>
2026-05-15 13:55:48 +0900

    kari

--- Assets/script/haikei.cs
+++ Assets/script/haikei.cs
@@ -5,6 +5,9 @@ public class haikei : MonoBehaviour
 {
     public float speed;
     public Transform[] basho;
+    public int bangou;
+    public int jougenn;
+    public bool infinity=false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,11 +17,21 @@ public class haikei : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(statichaikei.kaisuu >= bangou)
+        if (transform.position.y >= -11.8f)
         transform.Translate(new Vector3(0, speed, 0) * Time.deltaTime);
-        if (transform.position.y <= -10.8f) 
+        if (transform.position.y <= -10.8f && statichaikei.kaisuu >= jougenn && !infinity)
         {
+            statichaikei.kaisuu++;
+            Destroy(gameObject);
+        }
+        if (transform.position.y <= -10.8f&&statichaikei.kaisuu>=bangou) 
+        {
+           
+                statichaikei.kaisuu ++;
             transform.position = basho[0].position;
         }
-
+       
     }
 }
+
---
+++ Assets/script/statichaikei.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class statichaikei : MonoBehaviour
+{
+    public static int kaisuu=0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        kaisuu = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
