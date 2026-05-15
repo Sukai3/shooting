d105d29ed8cd6076a09ed73717df32081c4c0d68
2247004 <2247004@24HS04>
2026-05-15 13:57:02 +0900

    M系のシーンに音追加

--- Assets/script/Mselectmove.cs
+++ Assets/script/Mselectmove.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class Mselectmove : MonoBehaviour
@@ -7,27 +8,69 @@ public class Mselectmove : MonoBehaviour
     public int MAXS = 2;//ステージの最大数。
     public int MINS = 0;//ステージの最低数。
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public AudioClip sound1;
+    public AudioClip sound2;
+    public AudioClip sound3;
+    AudioSource audioSource;
+    private float timenow=0.0f;
+    private float Maxtime = 1.0f;
+    private bool a=false;
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow)&& Snow != MAXS)
+        if (Input.GetKeyDown(KeyCode.RightArrow)&& Snow != MAXS && a == false)
         {
+            audioSource.PlayOneShot(sound1);
             Snow++;
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && Snow != MINS)
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && Snow != MINS&&a==false)
         {
+            audioSource.PlayOneShot(sound1);
             Snow--;
         }
         selectplat();
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            nextscenemane();
+            //nextscenemane();
+            switch (Snow)
+            {
+                case 0:
+                    if (a == false)
+                    {
+                        a = true;
+                        audioSource.PlayOneShot(sound2);
+                        //for (; timenow == Maxtime; timenow += 0.1f) 
+                    }
+                    break;
+                default:
+                    audioSource.PlayOneShot(sound3);
+                    break;
+            }
         }
+        if (a) 
+        {
+            timenow += Time.deltaTime;
+        }
+        if (timenow > Maxtime) 
+        {
+            switch (Snow)
+            {
+                case 0:
+
+                    SceneManager.LoadScene("Samplescene");
+                    //for (; timenow == Maxtime; timenow += 0.1f) 
+                    break;
+                default:
+                   
+                    break;
+            }
+        }
+            //
     }
     void selectplat()//どのステージならどこに枠を置くのかを書いておいてる関数。名前は適当()
     {
@@ -52,9 +95,12 @@ public class Mselectmove : MonoBehaviour
         switch (Snow)
         {
             case 0:
+                audioSource.PlayOneShot(sound2);
+                //for (; timenow == Maxtime; timenow += 0.1f) ;
                 SceneManager.LoadScene("Samplescene");
                 break;
             default:
+                audioSource.PlayOneShot(sound3);
                 break;
         }
         
