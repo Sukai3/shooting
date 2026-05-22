8923f64424c2e8566977af16436c2b9f005b7114
2247004 <2247004@24HS04>
2026-05-22 14:47:09 +0900

    スコア地味実装

--- Assets/script/Number.cs
+++ Assets/script/Number.cs
@@ -10,6 +10,7 @@ public class Number : MonoBehaviour
     public int zurasi=0;
     public int i = 0;
     public float y = 0;
+    public int D = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,17 +20,31 @@ public class Number : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        one = sponer.enemysum % 10;
-        ten = (sponer.enemysum / 10) % 10;
-        //if (i == 0)
-        //{
-        //    i++;
-        //    Instantiate(number[thou], transform.position = new Vector3(-3, y, 0), transform.rotation);
-        //    Instantiate(number[hund], transform.position = new Vector3(-2.3f, y, 0), transform.rotation);
-        //    Instantiate(number[ten], transform.position = new Vector3(-1.5f, y, 0), transform.rotation);
-        //    Instantiate(number[one], transform.position = new Vector3(-0.7f, y, 0), transform.rotation);
-        //}
-        if (Input.GetKeyDown(KeyCode.Space))
+        switch (D)
+        {
+            case 1:
+                one = player.bunnsinn % 10;
+                ten = (player.bunnsinn / 10) % 10;
+                hund = (player.bunnsinn / 100) % 10;
+                thou = (player.bunnsinn / 1000) % 10;
+                break;
+            case 2:
+                one = sponer.enemysuu % 10;
+                ten = (sponer.enemysuu / 10) % 10;
+                hund = (sponer.enemysuu / 100) % 10;
+                thou = (sponer.enemysuu / 1000) % 10;
+                break;
+            case 3:
+
+                break;
+            default:
+                one = 7;
+                ten=7;
+                hund=7;
+                thou =7;
+                break;
+        }
+        if (i == 0)
         {
             i++;
             Instantiate(number[thou], transform.position = new Vector3(-3, y, 0), transform.rotation);
@@ -37,5 +52,13 @@ public class Number : MonoBehaviour
             Instantiate(number[ten], transform.position = new Vector3(-1.5f, y, 0), transform.rotation);
             Instantiate(number[one], transform.position = new Vector3(-0.7f, y, 0), transform.rotation);
         }
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    i++;
+        //    Instantiate(number[thou], transform.position = new Vector3(-3, y, 0), transform.rotation);
+        //    Instantiate(number[hund], transform.position = new Vector3(-2.3f, y, 0), transform.rotation);
+        //    Instantiate(number[ten], transform.position = new Vector3(-1.5f, y, 0), transform.rotation);
+        //    Instantiate(number[one], transform.position = new Vector3(-0.7f, y, 0), transform.rotation);
+        //}
     }
 }
